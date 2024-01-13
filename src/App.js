e80df34812a6d8b592f137c3866a451b793390a9
Ashutosh9599 <ashutoshdas091@gmail.com>
2024-01-13 15:00:20 +0530

    Cart Component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ProductContextProvider from './components/Context/ProductContextProvider';
 import ProductList from './components/Products/ProductList';
+import Header from './components/Layout/Header';
 
 
 const App = () => {
   return (
     <ProductContextProvider>
+      <Header/>
       <ProductList />
     </ProductContextProvider>
   );
