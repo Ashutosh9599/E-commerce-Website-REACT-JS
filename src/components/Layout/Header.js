25b5829e35867f46ba662ef4bcbf6539807d004b
Ashutosh9599 <ashutoshdas091@gmail.com>
2024-01-26 22:53:45 +0530

    Make chances

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,7 +2,7 @@ import React, { useState, useContext } from 'react';
 import { Navbar, Nav, Container, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
-import { Link } from 'react-router-dom'; 
+import { Link, useLocation } from 'react-router-dom'; 
 import './Header.css';
 import { useProductContext, AuthContext } from '../Context/ContextProvider';
 import Cart from '../Cart/Cart';
@@ -10,6 +10,9 @@ import Cart from '../Cart/Cart';
 const Header = () => {
     const { cart } = useProductContext();
     const [showCart, setShowCart] = useState(false);
+
+    const location = useLocation();
+    const isStoreRoute = location.pathname === '/store';
 
     const handleCartClick = () => {
         setShowCart(true);
@@ -42,6 +45,7 @@ const Header = () => {
                             )}
                             <Link to="/contact" className="nav-link" activeclassname="active">Contact-us</Link>
                         </Nav>
+                        {isStoreRoute && (
                         <Nav className="ml-auto">
                             <Button variant="outline-light" onClick={handleCartClick}>
                                 <FontAwesomeIcon icon={faShoppingCart} />
@@ -49,6 +53,7 @@ const Header = () => {
                                 <span className="cart-count">{totalCartItems}</span>
                             </Button>
                         </Nav>
+                         )}
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
